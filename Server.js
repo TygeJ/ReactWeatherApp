4954fbca008cf170f4e8e6b54b779c5fb867853a
Tyge <tygesjohnso@gmail.com>
2017-09-06 23:11:43 -0600

    Initial commit

---
+++ Server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Express server is up on port 3000');
+});
+
+//
+//  working on heroku deployment bellow 
+//
+// var express = require('express');
+//
+// var app = express();
+// const PORT = process.env.PORT || 3000;
+//
+// app.use(function (req, res, next) {
+//   if (req.headers['x-forwarded-proto'] === 'http') {
+//     next();
+//   } else {
+//     res.redirect('http://' + req.hostname + req.url);
+//   }
+// });
+//
+// app.use(express.static('public'));
+//
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port 3000' + PORT);
+// });
---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,25 @@
+var axios = require('axios'); // axios = promises, making http req
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=80a099d3ea5cd2ed2c42c9ddc1632664&units=imperial';
+// http://openweathermap.org/
+// custom api
+// unit specs from kelvin
+
+
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location); //URI = Uniform resource identifier, encodes URI to URL
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`; // jQuery convention
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) { // If valid location is not provided, send error, otherwise give temp
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
