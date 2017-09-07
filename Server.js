72084b21f4b8b5e555992a82141e617be51b11cc
Tyge <tygesjohnso@gmail.com>
2017-09-07 00:36:54 -0600

    prep repo for Heroku

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -16,3 +17,24 @@ app.use(express.static('public'));
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
 });
+
+
+
+// var express = require('express');
+//
+// var app = express();
+// const PORT = process.env.PORT || 3000;
+//
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'http') {
+//     next();
+//   } else {
+//     res.redirect('http://' + req.hostname + req.url);
+//   }
+// });
+//
+// app.use(express.static('public'));
+//
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port ' + PORT);
+// });
