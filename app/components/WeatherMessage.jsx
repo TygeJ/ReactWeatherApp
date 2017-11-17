288295599e5d84596b813d036d31921bf5334248
Tyge <tygesjohnso@gmail.com>
2017-11-17 11:41:04 -0700

    temp

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3> It is {temp}° Fahrenheit in {location}.</h3>
+    <h3> The temperature is {temp}° Fahrenheit in {location}.</h3>
   )
 }
 
